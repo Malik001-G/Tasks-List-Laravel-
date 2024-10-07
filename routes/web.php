9296c54e972f64f9e571e8d0876df4d36a58d185
Malik001-G <adisamalikng0@gmail.com>
2024-10-07 02:15:23 +0100

    Validated and stored the data in the db

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,28 @@ Route::get('/tasks', function () {
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
-
+Route::get('/tasks/{id}', function ($id) {
+    $task = Task::findOrFail($id);
+    return view('show', ['task' => $task]);
+})->name('tasks.show');
 Route::get('/tasks/{id}', function ($id) {
     $task = Task::findOrFail($id);
     return view('show', ['task' => $task]);
 })->name('tasks.show');
 
+
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully');
 })->name('tasks.store');
